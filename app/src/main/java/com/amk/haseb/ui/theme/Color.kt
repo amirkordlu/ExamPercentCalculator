052b282f205ac53dcbba38b702e6e6c8ff322f81
Amirmohammad Kordlu <amir.kordlu@gmail.com>
2024-01-30 21:11:04 +0330

    Set theme and font

--- app/src/main/java/com/amk/haseb/ui/theme/Color.kt
+++ app/src/main/java/com/amk/haseb/ui/theme/Color.kt
@@ -9,3 +9,14 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Theme Colors
+val Purple04 = Color(0xFF7E84F9)
+val Green05 = Color(0xFFB5F44F)
+val Yellow = Color(0xFFF8DF9F)
+
+// Semantic
+val GreenPositive = Color(0xFF09AC73)
+val RedNegative = Color(0xFFEF5350)
+val OrangeWarning = Color(0xFFFFB240)
+val BlueInfo = Color(0xFF29B6F6)
